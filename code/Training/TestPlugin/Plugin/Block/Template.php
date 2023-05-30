fe5c499b91e4305deb1e7da8bad7fc9ce6ba400f
max <maxmrljob@gmail.com>
2023-05-30 11:10:03 +0200

    1.8

---
+++ code/Training/TestPlugin/Plugin/Block/Template.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\TestPlugin\Plugin\Block;
+
+class Template
+{
+    public function afterToHtml(\Magento\Framework\View\Element\Template $subject, $result) {
+        if ($subject->getNameInLayout() == 'top.search') {
+            $result = '<div><p>' . $subject->getTemplate() . '</p>'
+                . '<p>' . get_class($subject) . '</p>' . $result . '</div>';
+        }
+        return $result;
+    }
+}
