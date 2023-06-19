c50d5bd1948a544d17dc52367ba58f49ec341022
max <maxmrljob@gmail.com>
2023-06-19 13:08:08 +0200

    3.4

---
+++ code/Training/Test/Plugin/Block/Product/View/Description.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Training\Test\Plugin\Block\Product\View;
+
+class Description extends \Magento\Catalog\Block\Product\View\Description
+{
+    /**
+     * @param \Magento\Catalog\Block\Product\View\Description $subject
+     * @return void
+     */
+    public function beforeToHtml(
+        \Magento\Catalog\Block\Product\View\Description $subject
+    ) {
+        $subject->getProduct()->setDescription('Test description1');
+    }
+
+}
